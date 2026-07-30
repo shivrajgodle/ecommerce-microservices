95edb69205346837eaba771488b526b9013fd018
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2026-07-30 15:50:57 +0530

    added Auth controller

--- identity-service/src/main/java/com/learning/identity_service/controller/AuthController.java
+++ identity-service/src/main/java/com/learning/identity_service/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.learning.identity_service.controller;
 import com.learning.identity_service.dto.request.LoginRequest;
 import com.learning.identity_service.dto.request.RefreshRequest;
 import com.learning.identity_service.dto.request.RegisterRequest;
+import com.learning.identity_service.dto.response.ApiResponse;
 import com.learning.identity_service.dto.response.AuthResponse;
 import com.learning.identity_service.service.AuthService;
 import jakarta.validation.Valid;
@@ -22,17 +23,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
-        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> register(@Valid @RequestBody RegisterRequest request){
+        AuthResponse result = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.CREATED.value(),"Registration successful",result));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
-        return ResponseEntity.ok(authService.login(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> login(@Valid @RequestBody LoginRequest request){
+        AuthResponse result = authService.login(request);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(),"Login successful", result));
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthResponse> refreshToken(@Valid @RequestBody RefreshRequest request){
-        return ResponseEntity.ok(authService.refresh(request));
+    public ResponseEntity<ApiResponse<AuthResponse>> refreshToken(@Valid @RequestBody RefreshRequest request){
+        AuthResponse result = authService.refresh(request);
+        return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK.value(),"Token refreshed", result));
     }
 }
